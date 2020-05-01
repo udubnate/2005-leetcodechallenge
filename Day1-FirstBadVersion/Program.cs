02c9bcb41d9c19359d48ad87e104b63bfafcb255
Nate Bruneau <nathbr@microsoft.com>
2020-05-01 09:59:00 -0700

    Binary Search solution

---
+++ Day1-FirstBadVersion/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Day1_FirstBadVersion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            int output = s.FirstBadVersion(100);
+            
+            Console.WriteLine("Result: " + output.ToString());
+        }
+    }
+/* The isBadVersion API is defined in the parent class VersionControl.
+      bool IsBadVersion(int version); */
+
+public class Solution {
+    public int FirstBadVersion(int n) {
+
+        var left = 1;
+        var right = n;
+
+        while (left < right) {
+            var mid = left + (right - left) / 2;
+            if (IsBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    bool IsBadVersion(int version) {
+        if (version >= 10){
+            return true;
+        } else {
+        return false;
+        }
+    }
+}
+
+
+}
