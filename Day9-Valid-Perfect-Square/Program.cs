234942cb5c438f4985a0489a069cd19b9ad64c39
udubnate <natebruneau@gmail.com>
2020-05-09 13:52:55 -0700

    O(n) solution

---
+++ Day9-Valid-Perfect-Square/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day9_Valid_Perfect_Square
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> test1 = new List<int>();
+            test1.Add(16); //return true
+            test1.Add(14); //return false
+            test1.Add(25); //return true
+            test1.Add(22); //return false;
+            test1.Add(1); //return true;
+            test1.Add(2147483647); //large number
+
+            Solution s = new Solution();
+
+            foreach (int test in test1)
+            {
+                bool result = s.IsPerfectSquare(test);
+                Console.WriteLine("Test: " + test + " Result = " + result.ToString());
+            }
+
+        }
+    }
+
+    public class Solution
+    {
+        public bool IsPerfectSquare(int num)
+        {
+            for (int i = 1; i <= num; i++)
+            {
+                double eval = (double)num / i;
+                if (eval == i)
+                {
+                    return true;
+                }
+                //trying to speed things up a bit
+                if (i > (num / 2)) break;
+
+            }
+            return false;
+        }
+    }
+}
