9ab56b77c786ce38c3681c25fe2d6312b7c5b8f2
udubnate <natebruneau@gmail.com>
2020-05-12 10:50:17 -0700

    push Day 11 and 12

---
+++ Day11-FloudFill/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Day11_FloudFill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+// solution took from https://leetcode.com/problems/flood-fill/discuss/626272/C-recursion-solution
+// didn't have time but wanted to submit a recursion one and analysis it
+    public class Solution {
+     public int[][] FloodFill(int[][] image, int sr, int sc, int newColor) {
+        var oldColor = image[sr][sc];
+        var rowMax = image.Length - 1;
+        var colMax = image[0].Length - 1;
+
+        if(oldColor != newColor) Fill(sr, sc);
+        return image;
+
+        void Fill(int row, int col) {
+            if(row < 0 || rowMax < row ||
+               col < 0 || colMax < col ||
+               image[row][col] != oldColor) return;
+
+            image[row][col] = newColor;
+
+            Fill(row - 1, col);
+            Fill(row + 1, col);
+            Fill(row, col - 1);
+            Fill(row, col + 1);
+        }
+    }
+}
+
+}
---
+++ Day12-SingleElementinaSortedArray/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day12_SingleElementinaSortedArray
+{
+    // sorted array, where we need to find a duplicate
+    // want to do O(log N) thinking Binary Search
+    // got a bit help here but he described it very well : https://www.youtube.com/watch?v=4Gi8uAz666s
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int[]> intList = new List<int[]>();
+            intList.Add(new int[] { 1, 1, 2, 3, 3, 4, 4, 8, 8 });
+            intList.Add(new int[] { 3, 3, 7, 7, 10, 11, 11 });
+            intList.Add(new int[] { 3 });
+            Solution s = new Solution();
+
+            foreach (int[] nums in intList)
+            {
+                int result = s.SingleNonDuplicate(nums);
+                Console.WriteLine(nums.ToString() + "Result: " + result);
+            }
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Solution
+    {
+        public int SingleNonDuplicate(int[] nums)
+        {
+            int left = 0;
+            int right = nums.Length - 1;
+
+            while (left < right)
+            {
+                int mid = left + (right - left) / 2; //helps with overflow for large numbers
+
+                bool isEven = (right - mid) % 2 == 0;
+                if (nums[mid] == nums[mid - 1])
+                {
+                    if (isEven)
+                    {
+                        right = mid - 2;
+                    }
+                    else
+                    {
+                        left = mid + 1;
+                    }
+                }
+                else if (nums[mid] == nums[mid + 1])
+                {
+                    if (isEven)
+                    {
+                        left = mid + 2;
+                    }
+                    else
+                    {
+                        right = mid - 1;
+                    }
+                }
+                else
+                {
+                    //return early if left and right are different
+                    return nums[mid];
+                }
+            }
+            return nums[left];
+        }
+    }
+
+}
