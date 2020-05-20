c2d0ae0802470b67dbbe08bf52224f4d54c9c99f
udubnate <natebruneau@gmail.com>
2020-05-20 13:20:37 -0700

    sending up a solution

---
+++ Day20-KthSmalestElementinaBST/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Day20_KthSmalestElementinaBST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    // leveraged this code today: https://leetcode.com/problems/kth-smallest-element-in-a-bst/discuss/63955/A-C-Inorder-recursive-solution
+    class Solution
+    {
+        // By mocking up StefanPochmann's C++ solution.
+        // https://leetcode.com/discuss/43267/4-lines-in-c
+        public int KthSmallest(TreeNode root, int k)
+        {
+            return helper(root, ref k);
+        }
+
+        int helper(TreeNode root, ref int k)
+        {
+            if (root == null) return 0;
+            int x = helper(root.left, ref k);
+            return k == 0 ? x : --k == 0 ? root.val : helper(root.right, ref k);
+        }
+    }
+}
+
+
+//Definition for a binary tree node.
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
